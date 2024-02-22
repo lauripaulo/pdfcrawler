14b2dee2520d8c341ef017211bd43d60301594d2
Lauri P. Laux Jr <lauripaulo@gmail.com>
2024-02-21 23:30:29 -0300

    Initial organization and engine basic functions.

--- engine.py
+++ engine.py
@@ -1,28 +1,18 @@
 import os
 from PyPDF2 import PdfReader
 import csv
+import logging
 
-def find_all_pdf_files(initial_path : str) -> list:
-    """
-    Find all PDF files in the specified directory and its subdirectories.
+logging.basicConfig(level=logging.DEBUG)
 
-    Args:
-        initial_path (str): The initial directory path to start the search.
 
-    Returns:
-        list: A list of dictionaries containing information about each PDF file found.
-              Each dictionary contains the following keys:
-              - fullname: The full path of the PDF file.
-              - size: The size of the PDF file in bytes.
-              - pages: The number of pages in the PDF file.
-              - info: Additional information about the PDF file (optional).
-    """
+def find_all_pdf_files(initial_path: str) -> list:
     pdf_files = []
-
     # Percorre a árvore de diretórios
     for root, dirs, files in os.walk(initial_path):
-        entry : dict
-        for file in files:           
+        entry: dict
+        logging.info(f"Searching in '{root}'...")
+        for file in files:
             # Verifica se o arquivo é um PDF
             if file.endswith('.pdf'):
                 entry = {
@@ -31,30 +21,43 @@ def find_all_pdf_files(initial_path : str) -> list:
                     "pages": 0,
                     "info": None
                 }
-                try:
-                    read_pdf_info(entry)
-                    pdf_files.append(entry)
-                    print(f"Found '{entry['fullname']}'...")
-                except Exception as e:
-                    print(f"Error reading {entry['fullname']}: {e}")
+                pdf_files.append(entry)
     return pdf_files
 
 
-def read_pdf_info(pdf_file : dict):
+def validate_pdfs(pdf_list: list) -> list:
+    for entry in pdf_list:
+        try:
+            read_pdf_info(entry)
+        except Exception as e:
+            logging.warning(f"Can't open {entry['fullname']}: {e}")
+    return pdf_list
+
+
+def read_pdf_info(pdf_file: dict) -> dict:
+    complete_entry: dict = pdf_file
     with open(pdf_file["fullname"], 'rb') as f:
         pdf = PdfReader(f)
         pdf_file["pages"] = len(pdf.pages)
         pdf_file["info"] = pdf.metadata
+        complete_entry = pdf_file
+        logging.info(
+            f"Valid PDF! Num pages: {pdf_file['pages']} path='{pdf_file['fullname']}'...")
+    return complete_entry
+
+
+def save_to_csv(pdf_list: list, filename: str) -> None:
+    with open(filename, "w", encoding='utf-8') as f:
+        writer = csv.DictWriter(
+            f, fieldnames=["fullname", "size", "pages", "info"])
+        writer.writeheader()
+        writer.writerows(pdf_list)
+        logging.info(f"CSV file '{filename}' saved successfully!")
 
 
 if __name__ == "__main__":
-    print("Finding pdfs...")
-    path = "c:\\"
-    pdf_files = find_all_pdf_files(path)
-    for entry in pdf_files:
-        print(entry)
-    with open("pdfs.txt", "w", encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=["fullname", "size", "pages", "info"])
-        writer.writeheader()
-        writer.writerows(pdf_files)
-    print("Done!")        
+    logging.info("Finding pdfs...")
+    path = "C:\\Users\\lauri\\Google Drive\\Pessoal\\# RPG"
+    files = find_all_pdf_files(path)
+    pdf_files = validate_pdfs(files)
+    save_to_csv(pdf_files, os.path.join(os.getcwd(), "pdfs.txt"))
---
+++ playground.py
@@ -0,0 +1,5 @@
+### This is a playground for testing and debugging purposes. ###
+
+if __name__ == "__main__":
+    print("Testing...")
+    print("Done!")        
