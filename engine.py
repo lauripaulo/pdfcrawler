9d1fdf9a07cc6cc6914c94792bdb9e56e97980be
Lauri P. Laux Jr <lauripaulo@gmail.com>
2024-02-22 11:58:49 -0300

    Simples callback oop refactoring

--- engine.py
+++ engine.py
@@ -6,52 +6,60 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-def find_all_pdf_files(initial_path: str) -> list:
-    pdf_files = []
-    # Percorre a árvore de diretórios
-    for root, dirs, files in os.walk(initial_path):
-        entry: dict
-        logging.info(f"Searching in '{root}'...")
-        for file in files:
-            # Verifica se o arquivo é um PDF
-            if file.endswith('.pdf'):
-                entry = {
-                    "fullname": os.path.join(root, file),
-                    "size": os.path.getsize(os.path.join(root, file)),
-                    "pages": 0,
-                    "info": None
-                }
-                pdf_files.append(entry)
-    return pdf_files
+CALLBACK_FILE_FOUND = 1
+CALLBACK_FILE_VALIDATED = 2
 
 
-def validate_pdfs(pdf_list: list) -> list:
-    for entry in pdf_list:
-        try:
-            read_pdf_info(entry)
-        except Exception as e:
-            logging.warning(f"Can't open {entry['fullname']}: {e}")
-    return pdf_list
+class CallBack:
+    def update(self, type: int, message: str) -> None:
+        logging.info(f"Type: {type} - Message: {message}")
 
 
-def read_pdf_info(pdf_file: dict) -> dict:
-    complete_entry: dict = pdf_file
-    with open(pdf_file["fullname"], 'rb') as f:
-        pdf = PdfReader(f)
-        pdf_file["pages"] = len(pdf.pages)
-        pdf_file["info"] = pdf.metadata
-        complete_entry = pdf_file
-        logging.info(
-            f"Valid PDF! Num pages: {pdf_file['pages']} path='{pdf_file['fullname']}'...")
-    return complete_entry
+class Finder:
+    pdf_files: list = []
+    validated_pdf_files: list = []
 
+    def find_all_pdf_files(self, initial_path: str, callback: CallBack) -> list:
+        # Percorre a árvore de diretórios
+        for root, dirs, files in os.walk(initial_path):
+            entry: dict
+            for file in files:
+                # Verifica se o arquivo é um PDF
+                if file.endswith('.pdf'):
+                    entry = {
+                        "fullname": os.path.join(root, file),
+                        "size": os.path.getsize(os.path.join(root, file)),
+                        "pages": 0,
+                        "info": None
+                    }
+                    self.pdf_files.append(entry)
+                    callback.update(CALLBACK_FILE_FOUND, f"Found '{entry}'...")
+        return self.pdf_files
 
-def save_to_csv(pdf_list: list, filename: str) -> None:
-    with open(filename, "w", encoding='utf-8') as f:
-        writer = csv.DictWriter(
-            f, fieldnames=["fullname", "size", "pages", "info"], dialect="excel")
-        writer.writeheader()
-        writer.writerows(pdf_list)
-        logging.info(f"CSV file '{filename}' saved successfully!")
+    def validate_pdfs(self, callback: CallBack) -> list:
+        for entry in self.pdf_files:
+            try:
+                self.read_pdf_info(entry)
+                self.validated_pdf_files.append(entry)
+                callback.update(CALLBACK_FILE_VALIDATED,
+                                f"Validated '{entry['fullname']}'...")
+            except Exception as e:
+                logging.warning(f"Can't open {entry['fullname']}: {e}")
+        return self.validated_pdf_files
 
+    def read_pdf_info(self, pdf_file: dict) -> dict:
+        complete_entry: dict = pdf_file
+        with open(pdf_file["fullname"], 'rb') as f:
+            pdf = PdfReader(f)
+            pdf_file["pages"] = len(pdf.pages)
+            pdf_file["info"] = pdf.metadata
+            complete_entry = pdf_file
+        return complete_entry
 
+    def save_to_csv(self, filename: str) -> None:
+        with open(filename, "w", encoding='utf-8') as f:
+            writer = csv.DictWriter(
+                f, fieldnames=["fullname", "size", "pages", "info"], dialect="excel")
+            writer.writeheader()
+            writer.writerows(self.validated_pdf_files)
+            logging.info(f"CSV file '{filename}' saved successfully!")
--- playground.py
+++ playground.py
@@ -1,4 +1,4 @@
-import engine as eng
+from engine import Finder, CallBack
 import logging
 import os
 
@@ -8,6 +8,9 @@ logging.basicConfig(level=logging.DEBUG)
 if __name__ == "__main__":
     logging.info("Finding pdfs...")
     path = "C:\\Users\\lauri\\Google Drive\\Pessoal\\# RPG"
-    files = eng.find_all_pdf_files(path)
-    pdf_files = eng.validate_pdfs(files)
-    eng.save_to_csv(pdf_files, os.path.join(os.getcwd(), "pdfs.txt"))
+    #path = "C:\\"
+    finder : Finder = Finder()
+    callback : CallBack = CallBack()
+    files = finder.find_all_pdf_files(path, callback=callback)
+    pdf_files = finder.validate_pdfs(callback=callback)
+    finder.save_to_csv(os.path.join(os.getcwd(), "pdfs.txt"))
