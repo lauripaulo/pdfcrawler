b3ad581f259d5f6307bbe0843d3e8bfa89ba4963
Lauri P. Laux Jr <lauripaulo@gmail.com>
2024-02-23 12:22:53 -0300

    Buttons state fix

--- pdfcrawler.py
+++ pdfcrawler.py
@@ -1,5 +1,6 @@
 import logging
 import threading
+from turtle import st
 import ttkbootstrap as tkb
 from ttkbootstrap.constants import *
 from tkinter import *
@@ -15,7 +16,6 @@ class PDFCrawler(tkb.Window):
         self.page_size_options = ["All", ">5", ">10", '>20']
         self.pdf_size_options = ["All", ">1MB", ">5MB", ">10MB"]
         self.finder = Finder()
-        self.folder : str = self.finder.get_current_folder()
 
         self.create_frame_folder(root)
         self.create_frame_filters(root)
@@ -72,20 +72,19 @@ class PDFCrawler(tkb.Window):
     def create_frame_folder(self, root: tkb.Window):
         self.frame_folder: tkb.Frame = tkb.Frame(root)
         self.lbl_folder = tkb.Label(
-            self.frame_folder, 
-            text="Folder:", 
+            self.frame_folder,
+            text="Folder:",
             width=10
         )
         self.lbl_folder.pack(side=LEFT, pady=5, padx=5)
         self.etr_folder = tkb.Entry(
-            self.frame_folder, 
-            textvariable=self.folder
+            self.frame_folder
         )
         self.etr_folder.insert(0, self.finder.get_current_folder())
         self.etr_folder.pack(side=LEFT, pady=5, padx=5, fill=X, expand=YES)
         self.btn_pick_folder = tkb.Button(
-            self.frame_folder, 
-            text="Choose folder", 
+            self.frame_folder,
+            text="Choose folder",
             bootstyle="primary",
             command=self.on_btn_pick_folder_click
         )
@@ -95,36 +94,36 @@ class PDFCrawler(tkb.Window):
     def create_frame_filters(self, root: tkb.Window):
         self.frame_filters: tkb.Frame = tkb.Frame(root)
         self.lbl_pages = tkb.Label(
-            self.frame_filters, 
-            text="Num. pages:", 
+            self.frame_filters,
+            text="Num. pages:",
             width=10
         )
         self.lbl_pages.pack(side=LEFT, pady=5, padx=5)
         self.cmb_page_size = tkb.Combobox(
-            self.frame_filters, 
-            values=self.page_size_options, 
+            self.frame_filters,
+            values=self.page_size_options,
             width=10
         )
         self.cmb_page_size.current(0)
         self.cmb_page_size.state(["readonly"])
         self.cmb_page_size.pack(side=LEFT, pady=5, padx=5)
         self.lbl_pdf_size = tkb.Label(
-            self.frame_filters, 
-            text="PDF size:", 
+            self.frame_filters,
+            text="PDF size:",
             width=10
         )
         self.lbl_pdf_size.pack(side=LEFT, pady=5, padx=5)
         self.cmb_pdf_size = tkb.Combobox(
-            self.frame_filters, 
-            values=self.pdf_size_options, 
+            self.frame_filters,
+            values=self.pdf_size_options,
             width=10
         )
         self.cmb_pdf_size.current(0)
         self.cmb_pdf_size.state(["readonly"])
         self.cmb_pdf_size.pack(side=LEFT, pady=5, padx=5)
         self.btn_find = tkb.Button(
-            self.frame_filters, 
-            text="Start search", 
+            self.frame_filters,
+            text="Start search",
             bootstyle="success",
             command=self.on_btn_find_click
         )
@@ -134,48 +133,61 @@ class PDFCrawler(tkb.Window):
     def create_action_menu(self, root: tkb.Window):
         self.frame_actions: tkb.Frame = tkb.Frame(root)
         self.btn_export_csv = tkb.Button(
-            self.frame_actions, 
-            text="Export to CSV", 
+            self.frame_actions,
+            text="Export to CSV",
             bootstyle="primary",
-            command=self.on_btn_export_csv_click
+            command=self.on_btn_export_csv_click,
+            state=DISABLED
         )
         self.btn_export_csv.pack(side=LEFT, pady=5, padx=5)
         self.btn_copy = tkb.Button(
-            self.frame_actions, 
-            text="Copy files to...", 
+            self.frame_actions,
+            text="Copy files to...",
             bootstyle="success",
-            command=self.on_btn_copy_click
+            command=self.on_btn_copy_click,
+            state=DISABLED
         )
         self.btn_copy.pack(side=LEFT, pady=5, padx=5)
         self.frame_actions.pack(anchor=E, fill=X, padx=5, pady=5)
 
+
     def on_btn_export_csv_click(self):
         print("Exporting to CSV...")
-    
+
     def on_btn_copy_click(self):
         print("Copying files...")
-        
+
     def on_btn_pick_folder_click(self):
         print("Picking folder...")
-        
+
     def on_btn_find_click(self):
+        self.btn_find.config(state=DISABLED)
+        self.btn_copy.config(state=DISABLED)
+        self.btn_export_csv.config(state=DISABLED)
+        self.btn_pick_folder.config(state=DISABLED)
         threading.Thread(target=self._run_find).start()
-        
+
     def _run_find(self):
-        print(f"Finding PDFs in {self.folder}...")
-        observer : CallBack = FileFinderObserver(self.progressbar, self.lbl_progress)
-        self.finder.find_all_pdf_files("/home/lauri", observer)
-        print("Done!")
-        
+        print(f"Finding PDFs in {self.etr_folder.get()}...")
+        observer: CallBack = FileFinderObserver(
+            self.progressbar, self.lbl_progress)
+        self.finder.find_all_pdf_files(self.etr_folder.get(), observer)
+        observer.update(CALLBACK_FILE_FOUND, "Done finding PDFs!")
+        self.btn_find.config(state=NORMAL)
+        self.btn_copy.config(state=NORMAL)
+        self.btn_export_csv.config(state=NORMAL)
+
+
 class FileFinderObserver(CallBack):
-    
+
     def __init__(self, progress_bar: tkb.Progressbar, label: tkb.Label) -> None:
-        self.progress_bar : tkb.Progressbar = progress_bar
-        self.label : tkb.Label = label
-        
+        self.progress_bar: tkb.Progressbar = progress_bar
+        self.label: tkb.Label = label
+
     def update(self, type: int, message: str) -> None:
         if type == CALLBACK_FILE_FOUND:
-          self.label.config(text=message)
+            self.label.config(text=message)
+
 
 if __name__ == "__main__":
     root = tkb.Window("PDFCrawler")
